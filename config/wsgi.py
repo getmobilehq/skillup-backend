ddd96682c0e49fa788a6917b56ad4d673bdd37f8
sirdesmond09 <sirdesmond09@gmail.com>
2022-01-24 18:39:31 +0000

    fixed env

--- config/wsgi.py
+++ config/wsgi.py
@@ -8,7 +8,6 @@ https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
 """
 import os
 
-
 configuration = os.getenv('ENVIRONMENT', 'development').title()
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 os.environ.setdefault('DJANGO_CONFIGURATION', configuration)
